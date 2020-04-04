b860950e9f4f7b4d2da4fae2712a0db2219e6a99
AFitting <asf45@humboldt.edu>
2020-04-03 20:28:03 -0700

    12:50

--- app/src/main/java/com/example/filterfeeders/MainActivity.java
+++ app/src/main/java/com/example/filterfeeders/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.filterfeeders;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,6 +20,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity
 {
     public static final int PERMISSION_CODE = 1000;
+    private static final int IMAGE_CAPTURE_CODE = 1001;
     Button photoBtn;
     ImageView photoView;
 
@@ -59,6 +64,14 @@ public class MainActivity extends AppCompatActivity
     }
 
     private void accessCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From Camera");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        //camera intent
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
     }
 
     //to handle permission result
@@ -78,5 +91,16 @@ public class MainActivity extends AppCompatActivity
                 }
             }
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //called when image captured from camera
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+
+        }
+
     }
 }
