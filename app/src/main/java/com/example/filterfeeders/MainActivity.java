1359e361a7ce5adbde812a738207785e9f0fc4be
AFitting <asf45@humboldt.edu>
2020-04-03 20:03:39 -0700

    9:37

--- app/src/main/java/com/example/filterfeeders/MainActivity.java
+++ app/src/main/java/com/example/filterfeeders/MainActivity.java
@@ -1,16 +1,82 @@
 package com.example.filterfeeders;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
+    public static final int PERMISSION_CODE = 1000;
+    Button photoBtn;
+    ImageView photoView;
+
+    Uri image_uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        photoView = findViewById(R.id.photo_view);
+        photoBtn = findViewById(R.id.photo_btn);
+
+        //when button is clicked
+        photoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // if system OS is marshmallow or higher, request runtime permission
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.CAMERA) ==
+                            PackageManager.PERMISSION_DENIED ||
+                            checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                            PackageManager.PERMISSION_DENIED) {
+                        //request permission if not enabled
+                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        //popup to request permissions
+                        requestPermissions(permission, PERMISSION_CODE);
+                    }
+                    else {
+                        //permission granted already
+                        accessCamera();
+                    }
+                }
+                else {
+                    //system OS is less than marshmallow
+                    accessCamera();
+                }
+            }
+        });
+    }
+
+    private void accessCamera() {
+    }
+
+    //to handle permission result
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //method called when user presses Allow or Deny from Permission Request Popup
+        switch (requestCode){
+            case PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED) {
+                    //permission granted from the popup
+                    accessCamera();
+                }
+                else {
+                    //permission denied from popup
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
